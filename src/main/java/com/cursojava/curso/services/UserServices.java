8f5f9e498df93917d34e27c4ec65e54b63c7810d
Juan Nobert <nobert407@gmail.com>
2022-03-23 15:09:35 -0300

    Exception handling - update

--- src/main/java/com/cursojava/curso/services/UserServices.java
+++ src/main/java/com/cursojava/curso/services/UserServices.java
@@ -3,6 +3,8 @@ package com.cursojava.curso.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -13,46 +15,51 @@ import com.cursojava.curso.repositories.UserRepository;
 import com.cursojava.curso.services.exceptions.DatabaseException;
 import com.cursojava.curso.services.exceptions.ResourceNotFoundException;
 
-
 @Service // Registra o componente no Spring como serviço
 public class UserServices {
-	
+
 	@Autowired
 	UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		/*
-		 * O Optional nos ajuda a evitar os erros NullPointerException, tirar a necessidade da verificação 
-		 * (if x != null) e também a escrever um código com menos linhas e mais bonito.
+		 * O Optional nos ajuda a evitar os erros NullPointerException, tirar a
+		 * necessidade da verificação (if x != null) e também a escrever um código com
+		 * menos linhas e mais bonito.
 		 */
-		Optional<User> obj =  userRepository.findById(id);
+		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User save(User obj) {
 		return userRepository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
 			userRepository.deleteById(id);
-		}catch(EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}
-		catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
-		
+
 	}
-	
+
 	public User update(Long id,User obj) {
-		User entity = userRepository.getById(id);
-		updateData(entity,obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getById(id);
+			updateData(entity,obj);
+			return userRepository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
+	
 		
 	}
 
@@ -60,8 +67,7 @@ public class UserServices {
 		entity.setNome(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
-	
-	
+
 }
